327846f0d3ace50be99b6aed2c591a0eea9cba9f
Yakutin Aleksandr <yakutinaleksandr@gmail.com>
2020-02-13 00:16:06 +0300

    added route and view for requisites, need to add some inputs for users

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Router, Route} from 'react-router';
+import createBrowserHistory from './helpers/history';
 import './App.css';
+import {Requisites} from "./components/requisites/Requisities";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router history={createBrowserHistory}>
+          <Route path='/requisites' component={Requisites}/>
+      </Router>
   );
 }
 
