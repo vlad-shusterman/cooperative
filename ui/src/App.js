863ed68a6318b2d7949caa43c97411f059c68087
Vlad Shusterman <vlad.shusterman30@gmail.com>
2020-02-27 02:02:31 +0300

    Feat(Register) : add ui, part of api

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,11 +3,13 @@ import {Router, Route} from 'react-router';
 import createBrowserHistory from './helpers/history';
 import './App.css';
 import {Requisites} from "./components/requisites/Requisities";
+import {Register} from "./components/register/Register";
 
 function App() {
   return (
       <Router history={createBrowserHistory}>
           <Route path='/requisites' component={Requisites}/>
+        <Route path='/register' component={Register}/>
       </Router>
   );
 }
