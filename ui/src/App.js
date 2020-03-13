680300bd996a269b3412d7acdb887936ca216c47
Stepan Ryzhevich <ryzhevichs@gmail.com>
2020-03-13 13:48:17 +0300

    Added UI

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import {Router, Route} from 'react-router';
+import {Route, Router} from 'react-router';
 import createBrowserHistory from './helpers/history';
 import './App.css';
 import {Requisites} from "./components/requisites/Requisities";
 import {Register} from "./components/register/Register";
+import MailSender from "./components/notifications/components/MailSender";
 
 function App() {
   return (
       <Router history={createBrowserHistory}>
           <Route path='/requisites' component={Requisites}/>
         <Route path='/register' component={Register}/>
+        <Route path='/notifications' component={MailSender}/>
       </Router>
   );
 }
---
+++ ui/src/components/notifications/components/MailSender.js
@@ -0,0 +1,132 @@
+import * as React from "react";
+import {Button, FormGroup} from "react-bootstrap";
+import {Container, Modal, ModalFooter} from "reactstrap";
+import Input from "reactstrap/es/Input";
+import Form from "react-bootstrap/Form";
+import ApiService from "../service/ApiService";
+import ModalHeader from "reactstrap/es/ModalHeader";
+import ModalBody from "reactstrap/es/ModalBody";
+
+export default class MailSender extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            mailParams: {
+                receivers: '',
+                subject: '',
+                text: '',
+                sendingType: 'EMAIL',
+                index: ''
+            },
+            showModal: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.sendMail = this.sendMail.bind(this);
+    }
+
+    handleChange({target}) {
+        this.setState(prevState => ({
+            mailParams: {
+                ...prevState.mailParams,
+                [target.name]: target.value
+            }
+        }))
+    }
+
+    sendMail() {
+        let receivers = this.state.mailParams.receivers;
+        let params = this.state.mailParams;
+        params.receivers = receivers.split(', ');
+        ApiService.sendEmail(params)
+            .then(response => {
+                if(response.status == 200) {
+                    this.setState(prevState => ({
+                        mailParams: {
+                            ...prevState.mailParams,
+                            receivers: '',
+                            subject: '',
+                            text: '',
+                            index: ''
+                        },
+                        showModal: true
+                    }));
+                }
+            })
+    }
+
+    render() {
+        return (
+            <Container className='col-md-4'>
+                <h2>Нотификации</h2>
+                <Form>
+                    <h3>Отправить сообщение</h3>
+                    sryzhevich@spanning.com
+                    <FormGroup>
+                        <label htmlFor='receivers'>Эл.адрес получателя</label>
+                        <Input
+                            name='receivers'
+                            className='receivers'
+                            value={this.state.mailParams.receivers}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor='subject'>Тема</label>
+                        <Input
+                            name='subject'
+                            className='subject'
+                            value={this.state.mailParams.subject}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor='emailText'>Тело сообщения</label>
+                        <textarea
+                            name='text'
+                            className='form-control text'
+                            value={this.state.mailParams.text}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor='sendingType'>Тип сообщения</label>
+                        <select
+                            name='sendingType'
+                            className='form-control sendingType'
+                            value={this.state.mailParams.sendingType}
+                            onChange={this.handleChange}
+                        >
+                            <option value='EMAIL' selected='selected'>Электронное</option>
+                            <option value='SIMPLE'>Бумажное</option>
+                        </select>
+                    </FormGroup>
+                    <FormGroup>
+                        <label htmlFor='index'>Индекс</label>
+                        <Input
+                            name='index'
+                            className='index'
+                            value={this.state.mailParams.index}
+                            onChange={this.handleChange}
+                        />
+                    </FormGroup>
+                    <Button
+                        type='button'
+                        onClick={this.sendMail}
+                    >Отправить</Button>
+                </Form>
+                    <Modal isOpen={this.state.showModal}>
+                        <ModalHeader  toggle={() => this.setState({showModal: false})} >Уведомление</ModalHeader>
+                        <ModalBody>
+                            <h4>Сообщение отправлено</h4>
+                        </ModalBody>
+                        <ModalFooter>
+
+                        </ModalFooter>
+                    </Modal>
+
+            </Container>
+        )
+    }
+}
